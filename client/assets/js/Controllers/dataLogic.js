c605e1c5c7f111cfc65bc5a5b9caa907eafa8d30
Bayarmagnai <bayarlvl@gmail.com>
2024-05-12 14:23:20 +0800

    uploading project

--- client/assets/js/Controllers/dataLogic.js
+++ client/assets/js/Controllers/dataLogic.js
@@ -4,14 +4,14 @@
             const mainCodeContainerRoute = window.parent.document.getElementById('render');
             const mainCodeContainer = mainCodeContainerRoute.contentWindow.document.getElementById('whitePage');
 
-            const mainCode = mainCodeContainer.outerHTML.toString();
+            const test = mainCodeContainer.outerHTML
             console.log(mainCodeContainer);
             const response = await fetch("http://localhost:3030/save-code",{ 
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({mainCode})
+                body: JSON.stringify({test})
             });
             if(!response.ok){
                 throw new Error(`HTTP error Status: ${response.status}`);
