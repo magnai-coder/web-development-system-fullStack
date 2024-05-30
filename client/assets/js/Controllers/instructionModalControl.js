c01899a16046fb6fe388dde069d6f31183e90f3f
Bayarmagnai <bayarlvl@gmail.com>
2024-05-31 01:42:49 +0800

    zasla

--- client/assets/js/Controllers/instructionModalControl.js
+++ client/assets/js/Controllers/instructionModalControl.js
@@ -6,10 +6,10 @@ var count = 0;
 
 
 const sources = [
-    "../assets/imgs/main/partsIn.PNG",
+    "../assets/imgs/main/parts.PNG",
     "../assets/imgs/main/buttons.PNG",
     "../assets/imgs/main/addingComponent.PNG",
-    "../assets/imgs/main/dragAndDrop.PNG",
+    "../assets/imgs/main/drag.PNG",
     "../assets/imgs/main/editing.PNG",
     "../assets/imgs/main/selectElement.PNG",
     "../assets/imgs/main/textEdit.PNG",
